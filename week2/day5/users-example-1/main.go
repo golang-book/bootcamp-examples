1a3ea8030cc3ec21a58afb4d07fde7d8b24491ac
Caleb Doxsey <caleb@doxsey.net>
2015-07-17 09:04:52 -0700

    login with admin

--- week2/day5/users-example-1/main.go
+++ week2/day5/users-example-1/main.go
@@ -11,17 +11,22 @@ import (
 func index(res http.ResponseWriter, req *http.Request) {
 	ctx := appengine.NewContext(req)
 	u := user.Current(ctx)
-	if u == nil {
-		url, _ := user.LoginURL(ctx, "/")
-		fmt.Fprintf(res, `<a href="%s">Sign in or register</a>`, url)
-		return
-	}
 	url, _ := user.LogoutURL(ctx, "/")
 	res.Header().Set("Content-Type", "text/html")
 	fmt.Fprintf(res, `Welcome, %s! (<a href="%s">sign out</a>)`, u, url)
 
 }
 
+func admin(res http.ResponseWriter, req *http.Request) {
+	ctx := appengine.NewContext(req)
+	u := user.Current(ctx)
+	url, _ := user.LogoutURL(ctx, "/")
+	res.Header().Set("Content-Type", "text/html")
+	fmt.Fprintf(res, `Welcome ADMIN, %s! (<a href="%s">sign out</a>)`, u, url)
+
+}
+
 func init() {
 	http.HandleFunc("/", index)
+	http.HandleFunc("/admin/", admin)
 }
