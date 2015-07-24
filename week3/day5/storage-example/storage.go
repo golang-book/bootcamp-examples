9e6a6019bd59921b3a2f061b088deb0ef7d879dc
Caleb Doxsey <caleb@doxsey.net>
2015-07-24 11:35:18 -0700

    storage example

--- week3/day5/storage-example/storage.go
+++ week3/day5/storage-example/storage.go
@@ -1,9 +1,11 @@
 package storage
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 
+	"golang.org/x/net/context"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
 
@@ -13,29 +15,63 @@ import (
 	"google.golang.org/cloud/storage"
 )
 
+const bucketName = "golang-bootcamp"
+
 func init() {
 	http.HandleFunc("/put", handlePut)
+	http.HandleFunc("/get", handleGet)
+	http.HandleFunc("/list", handleList)
+}
 
+func getCloudContext(appengineContext context.Context) context.Context {
+	hc := &http.Client{
+		Transport: &oauth2.Transport{
+			Source: google.AppEngineTokenSource(appengineContext, storage.ScopeFullControl),
+			Base:   &urlfetch.Transport{Context: appengineContext},
+		},
+	}
+
+	return cloud.NewContext(appengine.AppID(appengineContext), hc)
 }
 
 func handlePut(res http.ResponseWriter, req *http.Request) {
 	ctx := appengine.NewContext(req)
-	bucket := "golang-bootcamp"
+	cctx := getCloudContext(ctx)
 
-	hc := &http.Client{
-		Transport: &oauth2.Transport{
-			Source: google.AppEngineTokenSource(ctx, storage.ScopeFullControl),
-			Base:   &urlfetch.Transport{Context: ctx},
-		},
-	}
-
-	cctx := cloud.NewContext(appengine.AppID(ctx), hc)
-
-	writer := storage.NewWriter(cctx, bucket, "example.txt")
-	io.WriteString(writer, "Hello World!!!!!")
+	writer := storage.NewWriter(cctx, bucketName, "example.txt")
+	io.WriteString(writer, "Works")
 	err := writer.Close()
 	if err != nil {
 		http.Error(res, "ERROR WRITING TO BUCKET: "+err.Error(), 500)
 		return
+	}
+}
+
+func handleGet(res http.ResponseWriter, req *http.Request) {
+	ctx := appengine.NewContext(req)
+	cctx := getCloudContext(ctx)
+
+	rdr, err := storage.NewReader(cctx, bucketName, "example.txt")
+	if err != nil {
+		http.Error(res, err.Error(), 500)
+		return
+	}
+	defer rdr.Close()
+
+	io.Copy(res, rdr)
+}
+
+func handleList(res http.ResponseWriter, req *http.Request) {
+	ctx := appengine.NewContext(req)
+	cctx := getCloudContext(ctx)
+
+	var query *storage.Query
+	objs, err := storage.ListObjects(cctx, bucketName, query)
+	if err != nil {
+		http.Error(res, err.Error(), 500)
+		return
+	}
+	for _, obj := range objs.Results {
+		fmt.Fprintln(res, obj.Name)
 	}
 }
